48b9d8b461405e8492dff08e0fe5d546217c6559
Ohiomata BN <benjaminohiomatanathaniel@gmail.com>
2026-09-18 21:43:52 +0100

    add Footer component for the HomePage with copyright notice

---
+++ frontend/src/components/HomePage/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="py-8 bg-blue-700 text-white">
+      <div className="container text-center font-bold text-xl">
+        &copy; ReachOut 2026 All right reserved
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
