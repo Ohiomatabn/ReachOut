0ee75ed64b015c6637e289b9faac1c6204e18712
Ohiomata BN <benjaminohiomatanathaniel@gmail.com>
2026-09-22 23:10:27 +0100

    fix App component routes to correctly define AdminLoginForm and add AdminDashboard route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,15 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
 import AdminLoginForm from "./components/admin/Login";
 import ReportIssue from "./components/ReportIssue";
+import AdminDashboard from "./components/admin/AdminDashboard";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/report-issue" element={<ReportIssue />} />
-      <Route element={<AdminLoginForm />} path="/admin-login" />
+      <Route path="/admin-login" element={<AdminLoginForm />} />
+      <Route path="/admin-dashboard" element={<AdminDashboard />} />
     </Routes>
   );
 }
