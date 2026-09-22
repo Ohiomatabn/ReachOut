3c528f19f69c62b77f5091d1875add96ec2338b3
Ohiomata BN <benjaminohiomatanathaniel@gmail.com>
2026-09-22 23:01:43 +0100

    change Statistics component to accept summary prop and enhance layout

--- frontend/src/components/HomePage/Statistics.jsx
+++ frontend/src/components/HomePage/Statistics.jsx
@@ -1,20 +1,21 @@
-function Statistics() {
+function Statistics({ summary }) {
+  console.log(summary);
   return (
     <section className="pb-4">
       <div className="container">
         <h2>Statistics</h2>
-        <div className="flex justify-center items-center gap-8 flex-wrap">
-          <div className="sta-card">
-            <div className="font-bold text-4xl pb-2 text-blue-700">22</div>
-            Total Reports
+        <div className="flex flex-wrap justify-around items-center gap-4 mb-8">
+          <div className="sta-card bg-white">
+            <p className="text-sm text-slate-500">Total Reports</p>
+            <h3 className="text-3xl font-bold text-slate-900 mt-2">{10}</h3>
           </div>
-          <div className="sta-card">
-            <div className="font-bold text-4xl pb-2 text-blue-700">12</div>
-            Resolved Issues
+          <div className="sta-card bg-white">
+            <p className="text-sm text-slate-500">Unresolved</p>
+            <h3 className="text-3xl font-bold text-red-600 mt-2">{4}</h3>
           </div>
-          <div className="sta-card">
-            <div className="font-bold text-4xl pb-2 text-blue-700">10</div>
-            Unresolved Issues
+          <div className="sta-card bg-white">
+            <p className="text-sm text-slate-500">Resolved</p>
+            <h3 className="text-3xl font-bold text-emerald-600 mt-2">{6}</h3>
           </div>
         </div>
       </div>
