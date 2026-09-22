eafa3e8d4c8e30a43edc1f5dcf9d4628d7b4a0fe
Ohiomata BN <benjaminohiomatanathaniel@gmail.com>
2026-09-22 23:00:26 +0100

    change AdminLoginForm component to implement form handling with react-hook-form and improve layout

--- frontend/src/components/admin/Login.jsx
+++ frontend/src/components/admin/Login.jsx
@@ -1,13 +1,70 @@
+import NavBar from "../NavBar";
+import { useForm } from "react-hook-form";
+
 function AdminLoginForm() {
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = useForm({
+    loginEmail: "",
+    loginPassword: "",
+  });
+
+  function signInAdmin(data) {
+    try {
+      console.log(data);
+      reset();
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
-    <section>
-      <div className="container">
-        <form>
-          <input type="text" placeholder="Email Address" />
-          <input type="password" placeholder="Password" />
-        </form>
-      </div>
-    </section>
+    <>
+      <NavBar />
+      <section className="bg-gray-950 h-screen">
+        <div className="container flex justify-center items-center h-full">
+          <div className="w-75 m-auto bg-gray-100 pt-0 p-4 rounded-md">
+            <h3
+              className="text-2xl font-bold text-center py-6
+          "
+            >
+              Welcome Back.... Please Login
+            </h3>
+            <form onSubmit={handleSubmit(signInAdmin)}>
+              <input
+                type="text"
+                placeholder="Email Address"
+                className="input py-2 px-1 border rounded-sm w-full mb-4"
+                {...register("loginEmail", {
+                  required: "Email address is required",
+                })}
+              />
+              {errors.loginEmail && (
+                <p className="text-red-500 pb-2">{errors.loginEmail.message}</p>
+              )}
+              <input
+                type="password"
+                placeholder="Password"
+                className="input py-2 px-1 border rounded-sm w-full mb-4"
+                {...register("loginPassword", {
+                  required: "Password is required",
+                })}
+              />
+              {errors.loginPassword && (
+                <p className="text-red-500 pb-2">
+                  {errors.loginPassword.message}
+                </p>
+              )}
+              <button type="submit" className="btn w-full py-2">
+                {isSubmitting ? "Sign in" : "Login"}
+              </button>
+            </form>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
