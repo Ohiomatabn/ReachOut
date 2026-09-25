0e8d2ef8730977f39ffa29e30d759aee949cffa8
Ohiomata BN <benjaminohiomatanathaniel@gmail.com>
2026-09-25 20:51:02 +0100

    enhance NavBar component with consistent styling for links

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -4,18 +4,25 @@ function NavBar() {
   return (
     <nav className="py-5 fixed left-0 right-0 top-0 bg-white">
       <div className="container flex justify-between items-center">
-        <Link to="/">ReachOut</Link>
+        <Link to="/" className="text-2xl font-bold hover:text-blue-700">
+          ReachOut
+        </Link>
         <ul>
-          <li>
+          <li className="hover:text-blue-700 hover:font-bold">
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className="hover:text-blue-700 hover:font-bold">
             <Link to="/report-issue">Report an Issue</Link>
           </li>
-          <li>
+          <li className="hover:text-blue-700 hover:font-bold">
             <Link to="/admin-login">Admin</Link>
           </li>
-          <Link to="/admin-dashboard">Dashboard</Link>
+          <Link
+            to="/admin-dashboard"
+            className="hover:text-blue-700 hover:font-bold"
+          >
+            Dashboard
+          </Link>
         </ul>
       </div>
     </nav>
